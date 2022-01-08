88bfe511d760addfdb3ed96945329440fa76181f
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-07 17:39:12 -0700

    Add favoriting images, product name, color and a category button

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,17 +1,39 @@
 import "../ProductCard/ProductCard.css";
+import heart from '../../assets/transparent-heart.jpeg';
+import faveHeart from '../../assets/red-heart.png';
 
 const ProductCard = ({ id, name, color, img_url, category, favorite }) => {
   return (
     <article className="product-card">
       <div className="product-card-container">
         <img 
+          alt="favoriting heart"
+          src={ favorite ? faveHeart: heart }
+          className={ favorite ? "favorite-button": "unfavorite-button" }
+          onClick={() => {
+            if (favorite) {
+              //remove from favorites via id
+            } else {
+              //add to favorites via id
+            }
+          }}
+        />
+        <img 
           className="product-image"
           src={img_url} 
           alt={color + ` ` + name }
         />
-        <p className="product-name">
+        <h2 className="product-name">
           {name}
-        </p>
+        </h2>
+        <h2 className="product-color">
+          {color}
+        </h2>
+        <button
+          className={category}
+        >
+          Select {category}
+        </button>
       </div>
     </article>
   )
