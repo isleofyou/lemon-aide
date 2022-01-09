0de407b8104e32e93e728804599a68010ede57a0
Markus Rossio <argitavis@gmail.com>
2022-01-09 12:59:12 -0600

    Add a test for the products containers and product cards

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -13,4 +13,23 @@ describe('Lemon-aide dashboard test', () => {
       .get('img[class="logo-img"]')
       .should('have.length', '1');
   });
+
+  it('As a user when I visit the homepage I should see all the products', () => {
+    cy.get('main[class="products-container"]')
+      .should('have.length', 1)
+      .get('article[class="product-card"]')
+      .should('have.length', 3);
+
+    cy.get('article[class="product-card"]')
+      .get('img[class="unfavorite-button"]')
+      .should('have.length', 3)
+      .get('img[class="product-image"]')
+      .should('have.length', 3)
+      .get('h2[class="product-name"]')
+      .should('have.length', 3)
+      .get('h2[class="product-color"]')
+      .should('have.length', 3)
+      .get('button')
+      .should('have.length', 3);
+  });
 });
