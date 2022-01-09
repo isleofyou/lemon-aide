e722c92534f320afb71e4d14a1127068120aa212
Markus Rossio <argitavis@gmail.com>
2022-01-09 12:25:48 -0600

    Add before each block and test for header

---
+++ cypress/integration/dashboard_spec.js
@@ -0,0 +1,13 @@
+describe('Lemon-aide dashboard test', () => {
+  beforeEach(() => {
+    cy.intercept('GET', '/api/v1/all-products', {
+      fixture: 'products.json'
+    });
+    cy.visit('http://localhost:3000');
+  })
+
+  it('As a user when I visit the homepage I should see a header', () => {
+    cy.get('header[class="header-container"]')
+      .should('have.length', 1);
+  });
+});
