127014e4ac93bbede66eb62e5c2d350ff747ee5b
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-10 09:16:33 -0700

    Refactor the ProductsContainer component to useLocation to conditionally render

--- src/Components/ProductsContainer/ProductsContainer.js
+++ src/Components/ProductsContainer/ProductsContainer.js
@@ -1,8 +1,14 @@
 import ProductCard from '../ProductCard/ProductCard';
 import './ProductsContainer.css';
 import Loading from '../Loading/Loading';
+import brokenHeart from '../../assets/broken-heart.png';
+import { useLocation } from 'react-router-dom';
 
 const ProductsContainer = ({ products, addFavorite }) => {
+  let location = useLocation();
+  console.log('location 1', location);
+  //if location.pathname === "/" => render usual w/Loading component
+  //if location.pathname === "/api/v1/favorites" => render w/broken heart div
   const productCards = products.map(product => {
     return (
       <ProductCard 
@@ -19,11 +25,22 @@ const ProductsContainer = ({ products, addFavorite }) => {
   });
 
   return (
-    productCards.length ? 
-      <main className="products-container">
-        {productCards}
-      </main> 
-    : <Loading />
+    location.pathname === "/" ?
+      productCards.length ?
+        <main className="products-container">
+          {productCards}
+        </main> 
+      : <Loading />
+    : 
+      productCards.length && products.every(product => product.favorite) ?
+        <main className="favorite-products-container">
+          {productCards}
+        </main> 
+    : <div className="no-favorites">
+        <img className="heart-picture" src={brokenHeart} alt="broken heart" />
+        <h3>Add favorites to see them here.</h3>
+      </div>
+
   );
 }
 
