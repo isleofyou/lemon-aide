9a82a4aa5edbe4d27536cbc42475f7a8b0b9c484
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-11 15:37:55 -0700

    Refactor CartDropdown component to create outfitItemCards based on the selectedOutfitItemIds

--- src/Components/CartDropdown/CartDropdown.js
+++ src/Components/CartDropdown/CartDropdown.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import './CartDropdown.css';
+import OutfitItemCard from '../OutfitItemCard/OutfitItemCard';
 
 const CartDropdown = ({ toggleCart, outfitItems, deleteItemFromOutfit, addOutfit, products }) => {
-  //this will display our outfitItemCards
+  const selectedOutfitItemIds = Object.values(outfitItems).filter(item => item !== null);
+  const matchingProducts = products.filter(product => selectedOutfitItemIds.includes(product.id));
+
+  const outfitItemCards = matchingProducts.map(product => {
+    return <OutfitItemCard 
+            key={product.id}
+            name={product.name}
+            color={product.color}
+            img_url={product.img_url}
+            category={product.category}
+            deleteItemFromOutfit={deleteItemFromOutfit}
+          />
+  });
+
   return (
     <div className='cart-dropdown'>
       <button onClick={() => toggleCart()}>X</button>
+      {outfitItemCards}
       <button 
         className='create-outfit-button'
         onClick={() => {
           addOutfit();
+          toggleCart();
         }}
       >
       Save Outfit
