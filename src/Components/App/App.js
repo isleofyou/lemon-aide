560b0439ded75e0e9a6d50421d8889fcaab7abcd
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-11 08:27:04 -0700

    Add wireframe for deleteOutfit method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -87,6 +87,19 @@ class App extends Component {
       });
   }
 
+  //this will need to be passed down as props to the container that holds the outfits (create new outfitsContainer component that will display each outfit via outfitCard)
+  deleteOutfit = (id) => {
+    return deleteOutfit(id)
+      .then(deletedOutfitId => {
+        //this will return the id of the deleted outfit
+        console.log(`deletedOutfitId from backend:`, deletedOutfitId.result.id)        
+        // const updatedOutfitItems = [...this.state.outfits].filter(outfitId => outfitId !== deletedOutfitId);
+        //will need to setState for the outfits array
+        //this.setState({ outfits: updatedOutfitItems })?
+;      })
+      .catch(error => {
+        this.setState({ error: error.message });
+      });
   }
 
   render = () => {
