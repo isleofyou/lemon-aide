3ad8408ab022ad7a67fc7b629dbdb3dc7ed60623
David Tran <frozenpho@gmail.com>
2022-01-11 11:20:07 -0700

    Update Ivonne's github img

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,6 +3,7 @@ import './Footer.css';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../../assets/Lululemon_Athletica_logo.png';
 import * as AiIcons from 'react-icons/ai';
+import cat from '../../assets/cat.png';
 
 const Footer = () => {
   return (
@@ -14,7 +15,7 @@ const Footer = () => {
               <div className='contact-info'>
                 <Link to='/'>Ivonne Hernandez</Link>
                 <a className='contact-icons' href={'https://www.linkedin.com/in/ivonne-hernandez-107b0324/'} target={'blank'}><AiIcons.AiOutlineLinkedin /></a>
-                <a className='contact-icons' href={'https://github.com/ivonne-hernandez'} target={'blank'}><AiIcons.AiOutlineGithub /></a>
+                <a className='contact-icons' href={'https://github.com/ivonne-hernandez'} target={'blank'}><img className='cat' alt='Ivonne octocat' src={cat}/></a>
               </div>
               <div className='contact-info'>
                 <Link to='/'>David Tran</Link>
