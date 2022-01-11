274a3d0410171888680dc6d6bf94532c3eb0bde2
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-11 10:56:17 -0700

    Refactor Header component's toggleCart method name and formatting

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,26 +3,35 @@ import logo from '../../assets/Lululemon_Athletica_logo.png';
 import './Header.css';
 import Aside from '../Aside/Aside';
 import CartDropdown from '../CartDropdown/CartDropdown';
-import CartIcon from '../CartIcon/CartIcon';
+import OutfitCartButton from '../OutfitCartButton/OutfitCartButton';
 
 const Header = () => {
   const [hidden, setHidden] = useState(true); 
 
-  const toggleCartHidden = () => {
-    setHidden(!hidden)
+  const toggleCart = () => {
+    setHidden(!hidden);
   }
 
   return (
     <header className='header-container'>
       <Aside />
-      <img className='logo-img' alt='Lulu Lemon Logo' src={logo}/>
+      <img 
+        className='logo-img' 
+        alt='Lulu Lemon Logo' 
+        src={logo}
+      />
       <h1>Lemon Aide</h1>
-      <CartIcon toggleCartHidden={toggleCartHidden}/>
-      {
-        hidden ? null : <CartDropdown cartItems={[]} toggleCartHidden={toggleCartHidden}/>
+      <OutfitCartButton 
+        toggleCart={toggleCart}
+      />
+      { hidden ? null 
+        : 
+        <CartDropdown 
+          toggleCart={toggleCart}
+        />
       }
     </header>
-  )
+  );
 };
 
 export default Header;
