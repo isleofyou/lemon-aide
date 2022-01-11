e4f3ad0ae9d1aa99315bc50d23f206f5f7704374
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-11 15:42:51 -0700

    Add a fully functional OutfitItemCard component and styling

--- src/Components/OutfitItemCard/OutfitItemCard.js
+++ src/Components/OutfitItemCard/OutfitItemCard.js
@@ -1,10 +1,26 @@
 import '../OutfitItemCard/OutfitItemCard.css';
 
-const OutfitItemCard = ({ products, outfitItems, deleteItemFromOutfit }) => {
-  const idsOfOutfitItems = Object.values(outfitItems);
-  const productsForOutfitItems = outfitItems.
-
+const OutfitItemCard = ({ name, color, img_url, category, deleteItemFromOutfit }) => {
   return (
+    <article>
+      <img 
+        className='outfit-item-image'
+        src={img_url}
+        alt={color + ` ` + name }
+      />
+      <h2 className="outfit-item-name">
+          {name}
+        </h2>
+      <h2 className="outfit-item-color">
+        {color}
+      </h2>
+      <button
+        className='remove-item-card'
+        onClick={() => deleteItemFromOutfit(category)}
+      >
+      X
+      </button>
+    </article>
 
   );
 
