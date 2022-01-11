867c87839dab051b8205229a8067b8a1ef48f611
David Tran <frozenpho@gmail.com>
2022-01-11 13:22:06 -0700

    Add home page click function

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,13 +2,17 @@ import React from 'react';
 import logo from '../../assets/Lululemon_Athletica_logo.png';
 import './Header.css';
 import Aside from '../Aside/Aside';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+
+  let navigate = useNavigate();
+  
   return (
     <header className='header-container'>
       <Aside />
-      <img className='logo-img' alt='Lulu Lemon Logo' src={logo}/>
-      <h1>Lemon Aide</h1>
+        <img className='logo-img' alt='Logo' src={logo} onClick={() => {navigate('/')}}/>
+        <h1 onClick={() => {navigate('/')}}>Lemon Aide</h1>
     </header>
   )
 };
