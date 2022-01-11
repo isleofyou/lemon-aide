caef2ee8d3b4e44cd321bc05c0783216f99ba7c4
Ivonne Hernandez <ivonne702@gmail.com>
2022-01-11 10:16:26 -0700

    Add wireframe for OutfitItemCard and add styling file

---
+++ src/Components/OutfitItemCard/OutfitItemCard.js
@@ -0,0 +1,13 @@
+import '../OutfitItemCard/OutfitItemCard.css';
+
+const OutfitItemCard = ({ products, outfitItems, deleteItemFromOutfit }) => {
+  const idsOfOutfitItems = Object.values(outfitItems);
+  const productsForOutfitItems = outfitItems.
+
+  return (
+
+  );
+
+}
+
+export default OutfitItemCard;
